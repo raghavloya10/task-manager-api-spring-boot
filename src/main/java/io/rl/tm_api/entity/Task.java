ce50f5d3d98484ca81fd0d991eadbfcbf676c474
unknown <raghavloya100@gmail.com>
2021-05-09 13:06:03 +0530

    Adding lombok to the current project

--- src/main/java/io/rl/tm_api/entity/Task.java
+++ src/main/java/io/rl/tm_api/entity/Task.java
@@ -10,13 +10,20 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
+@Getter @Setter @ToString @AllArgsConstructor @RequiredArgsConstructor
 @Table(name = "tasks")
 public class Task {
 
 	@Column(name = "id")
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
 	@Column
@@ -32,69 +39,5 @@ public class Task {
 	@Column
 	private Boolean completed=false;
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public Boolean getCompleted() {
-		return completed;
-	}
-
-	public void setCompleted(Boolean completed) {
-		this.completed = completed;
-	}
-
-	public Task(int id, String title) {
-		super();
-		this.id = id;
-		this.title = title;
-	}
-
-	public Task(int id, Boolean completed) {
-		super();
-		this.id = id;
-		this.completed = completed;
-	}
-	
-	public Task() {
-		
-	}
-	
-	public Task(String title) {
-		this.title = title;
-	}
-	
-	public int getUserId() {
-		if(user == null) {
-			return 0;
-		}
-		return user.getId();
-	}
-
-	@Override
-	public String toString() {
-		return "Task [title=" + title + ", completed=" + completed + "]";
-	}
-	
 }
 
--- src/main/java/io/rl/tm_api/entity/User.java
+++ src/main/java/io/rl/tm_api/entity/User.java
@@ -1,6 +1,5 @@
 package io.rl.tm_api.entity;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -12,127 +11,141 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
+@RequiredArgsConstructor @ToString
 @Table(name = "users")
 public class User {
 
+	@Getter @Setter 
 	@Column(name = "first_name")
 	private String firstName;
 	
+	@Getter @Setter 
 	@Column(name = "last_name")
 	private String lastName;
 	
+	@Getter @Setter 
 	@Column(name = "id")
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@Getter @Setter 
 	@Column(name = "username")
 	private String username;
 	
+	@Getter @Setter 
 	@Column(name = "password")
 	private String password;
 	
+	@Getter @Setter 
 	@Column(name = "active")
-	private int active;
+	private int active=1;
 	
+	@Getter @Setter 
 	@Column(name = "roles")
 	private String roles;
 	
+	@Setter 
 	@OneToMany(mappedBy = "user", cascade = {
 					CascadeType.DETACH, CascadeType.MERGE, 
 					CascadeType.PERSIST, CascadeType.REFRESH
 				})
 	private List<Task> tasks;
 
-	public String getRoles() {
-		return roles;
-	}
+	// public String getRoles() {
+	// 	return roles;
+	// }
 
-	public void setRoles(String roles) {
-		this.roles = roles;
-	}
+	// public void setRoles(String roles) {
+	// 	this.roles = roles;
+	// }
 
-	public String getFirstName() {
-		return firstName;
-	}
+	// public String getFirstName() {
+	// 	return firstName;
+	// }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+	// public void setFirstName(String firstName) {
+	// 	this.firstName = firstName;
+	// }
 
-	public String getLastName() {
-		return lastName;
-	}
+	// public String getLastName() {
+	// 	return lastName;
+	// }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+	// public void setLastName(String lastName) {
+	// 	this.lastName = lastName;
+	// }
 
-	public int getId() {
-		return id;
-	}
+	// public int getId() {
+	// 	return id;
+	// }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+	// public void setId(int id) {
+	// 	this.id = id;
+	// }
 
-	public String getUsername() {
-		return username;
-	}
+	// public String getUsername() {
+	// 	return username;
+	// }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+	// public void setUsername(String username) {
+	// 	this.username = username;
+	// }
 
-	public String getPassword() {
-		return password;
-	}
+	// public String getPassword() {
+	// 	return password;
+	// }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	// public void setPassword(String password) {
+	// 	this.password = password;
+	// }
 
-	public int getActive() {
-		return active;
-	}
+	// public int getActive() {
+	// 	return active;
+	// }
 
-	public void setActive(int active) {
-		this.active = active;
-	}
+	// public void setActive(int active) {
+	// 	this.active = active;
+	// }
 	
-	public void addTask(Task task) {
-		if(this.tasks == null) {
-			this.tasks = new ArrayList<>();
-		}		
-		this.tasks.add(task);
-	}
+	// public void addTask(Task task) {
+	// 	if(this.tasks == null) {
+	// 		this.tasks = new ArrayList<>();
+	// 	}		
+	// 	this.tasks.add(task);
+	// }
 	
-	public void setTasks(List<Task> tasks) {
-		this.tasks = tasks;
-	}
+	// public void setTasks(List<Task> tasks) {
+	// 	this.tasks = tasks;
+	// }
 	
-	public User(String username, String password) {
-		this.username = username;
-		this.password = password;
-	}
+	// public User(String username, String password) {
+	// 	this.username = username;
+	// 	this.password = password;
+	// }
 
-	public User() {
+	// public User() {
 		
-	}	
+	// }	
 
-	public User(User u) {
-		this.id = u.getId();
-		this.username = u.getUsername();
-		this.firstName = u.getFirstName();
-		this.lastName = u.getLastName();
-		this.password = u.getPassword();
-		this.active = u.getActive();
-		this.roles = u.getRoles();
-	}
+	// public User(User u) {
+	// 	this.id = u.getId();
+	// 	this.username = u.getUsername();
+	// 	this.firstName = u.getFirstName();
+	// 	this.lastName = u.getLastName();
+	// 	this.password = u.getPassword();
+	// 	this.active = u.getActive();
+	// 	this.roles = u.getRoles();
+	// }
 	
-	@Override
-	public String toString() {
-		return username+" - "+firstName+lastName;
-	}
+	// @Override
+	// public String toString() {
+	// 	return username+" - "+firstName+lastName;
+	// }
 }
